e9440ff0513d19fe8942344e7805277736505877
ucciefmhmd <uccief18@gmail.com>
2024-04-07 15:25:46 +0200

    update student

---
+++ LMS.BL/Interface/IChooseQuestionRep.cs
@@ -0,0 +1,16 @@
+﻿using LMS.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BL.Interface
+{
+    public interface IChooseQuestionRep
+    {
+        void Add(ChooseQuestion chooseQues);
+        void update(ChooseQuestion chooseQues);
+        void Delete(ChooseQuestion chooseQues);
+    }
+}
---
+++ LMS.BL/Repository/ChooseQuestionRep.cs
@@ -0,0 +1,38 @@
+﻿using LMS.BL.Interface;
+using LMS.DAL.Database;
+using LMS.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BL.Repository
+{
+    public class ChooseQuestionRep : IChooseQuestionRep
+    {
+        private readonly LMSContext db;
+
+        public ChooseQuestionRep(LMSContext db)
+        {
+            this.db = db;
+        }
+        public void Add(ChooseQuestion chooseQues)
+        {
+            db.ChooseQuestion.Add(chooseQues);
+            db.SaveChanges();
+        }
+
+        public void Delete(ChooseQuestion chooseQues)
+        {
+            db.ChooseQuestion.Remove(chooseQues);
+            db.SaveChanges();
+        }
+
+        public void update(ChooseQuestion chooseQues)
+        {
+            db.ChooseQuestion.Update(chooseQues);
+            db.SaveChanges();
+        }
+    }
+}
